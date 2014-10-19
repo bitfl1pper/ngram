26f3fdd03382c54da483a4d3186c63eb1c05b87d
bitfl1pper <bitfl1pper@users.noreply.github.com>
2014-10-18 22:13:52 -0400

    added an easier freq fn i was writing alot

--- src/ngram/util.clj
+++ src/ngram/util.clj
@@ -4,3 +4,9 @@
   "Drops values from a collection if they are equal to "
   [val col]
   (filter #(not= val %) col))
+
+(defn seefreq
+  "Takes the frequencies of items in a collection, sorts
+   the resulting collection by #, and reverses the list."
+  [col]
+  (reverse (sort-by val (frequencies col))))
